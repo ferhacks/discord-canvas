70fc7cc7cab7c16174697e9c2d726680e70871c2
ferhacks <63879582+ferhacks@users.noreply.github.com>
2022-01-02 02:42:47 +0100

    Update Goodbye.js

--- src/greetings/Goodbye.js
+++ src/greetings/Goodbye.js
@@ -3,8 +3,8 @@ const Greeting = require("./Base");
 module.exports = class Goodbye extends Greeting {
     constructor() {
         super();
-        this.textTitle = "GOODBYE";
-        this.textMessage = "Leaving {server}";
+        this.textTitle = "Hasta luego!";
+        this.textMessage = "Dejando {server}";
         this.colorTitle = "#df0909";
     }
 };
