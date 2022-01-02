8caea98eda5e47bcbff342a6b87fd4d6bc13e28a
ferhacks <63879582+ferhacks@users.noreply.github.com>
2022-01-02 02:42:18 +0100

    Update Welcome.js

--- src/greetings/Welcome.js
+++ src/greetings/Welcome.js
@@ -3,8 +3,8 @@ const Greeting = require("./Base");
 module.exports = class Welcome extends Greeting {
     constructor() {
         super();
-        this.textTitle = "WELCOME";
-        this.textMessage = "Welcome to {server}";
+        this.textTitle = "Bienvenido!";
+        this.textMessage = "Bienvenido a {server}!";
         this.colorTitle = "#03A9F4";
     }
 };
